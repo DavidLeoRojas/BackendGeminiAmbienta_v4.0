39ba753b213781ad3a988287d384cd88f29fd006
@DavidLeoRojas <david.rojas@uptc.edu.co>
2025-11-07 20:19:57 -0500

    v.92

--- src/main/java/com/gemini/gemini_ambiental/config/CorsConfig.java
+++ src/main/java/com/gemini/gemini_ambiental/config/CorsConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 @Configuration
 public class CorsConfig {
 
@@ -13,9 +14,9 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
+                        // Lista explícita de orígenes permitidos
                         .allowedOriginPatterns(
-                                "http://localhost:8080",
-                                "http://localhost:3000",
+                                "http://localhost:*",
                                 "https://davidleorojas.github.io",
                                 "https://backendgeminiambienta-v4-0.onrender.com"
                         )
@@ -26,4 +27,3 @@ public class CorsConfig {
         };
     }
 }
-
