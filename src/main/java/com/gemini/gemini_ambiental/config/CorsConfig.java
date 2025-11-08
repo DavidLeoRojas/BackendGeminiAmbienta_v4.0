5236d82a78575fd37cbcb909da99cae466653213
@DavidLeoRojas <david.rojas@uptc.edu.co>
2025-11-07 20:09:53 -0500

    v.91

--- src/main/java/com/gemini/gemini_ambiental/config/CorsConfig.java
+++ src/main/java/com/gemini/gemini_ambiental/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins(
+                        .allowedOriginPatterns(
                                 "http://localhost:8080",
                                 "http://localhost:3000",
                                 "https://davidleorojas.github.io",
